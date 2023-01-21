defcab8c88bab9d1e1446fab8aff7e1e40ffcc65
pretorianin <45599615+pretorianin@users.noreply.github.com>
2023-01-21 06:17:24 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+let activeSlideNumber = 1;
+let arrowLeft = document.querySelector('.arrow-left');
+let arrowRight = document.querySelector('.arrow-right')
+
+
+let hideactiveSlide = () => {
+    let activeElement = document.querySelector('.active');
+    activeElement.classList.remove('active');
+};
+
+
+let showSlide = (slideNumber) => {
+    hideactiveSlide();
+    document.querySelector('#slide'+slideNumber).classList.add('active');
+};
+
+
+let showNextSlide = () => {
+    if(activeSlideNumber === 3){
+        activeSlideNumber = 1;
+    } else {
+        activeSlideNumber += 1;
+    }
+    showSlide(activeSlideNumber);
+};
+
+let showPreviousSlide = () => {
+    if(activeSlideNumber === 1){
+        activeSlideNumber = 3;
+    } else {
+        activeSlideNumber -= 1;
+    }
+    showSlide(activeSlideNumber);
+};
+
+
+for(let i = 1; i <= 3; i++){
+    let showSlideI = () => {
+        activeSlideNumber = i;
+        showSlide(i);
+    };
+    document.querySelector('#dot'+i).addEventListener('click', showSlideI);
+}
+
+
+arrowRight.addEventListener('click', showNextSlide);
+arrowLeft.addEventListener('click', showPreviousSlide);
